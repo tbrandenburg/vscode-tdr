9ab0c56b6ccf62221b860a10e3bd6c038fbea9ba
Tom Brandenburg <tom.brandenburg@gmx.de>
2023-02-22 22:18:55 +0100

    Added technical debt addition command

--- src/techdebt.ts
+++ src/techdebt.ts
@@ -2,52 +2,30 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as os from 'os';
+import * as crypto from 'crypto';
 
 import { TechDebtSidebar } from './webview/sidebar';
 
-class TechDebt {
-    id: string = "";
-    brief: string = "";
-    author: string = "";
-    date: string = "";
-    description: string = "";
-    owner: string = "";
-    category: string = "techdebt";
-    severity: string = "warning";
-    priority: string = "";
-    file: string = ".";
-    line: number = 0;
-    column: number = 0;
-    votes: number = 1;
-    workitem: string = "";
-    cost: string = "";
-    effort: string = "";
-    impedes: string = "";
-    discussion: any[] = [];
-    tags: string[] = [];
-
-    constructor(brief: string) {
-
-        const currentDate = new Date();
-
-        const day = currentDate.getDate().toString().padStart(2, "0");
-        const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
-        const year = currentDate.getFullYear().toString();
-
-        const formattedDate = `${year}-${month}-${day}`;
-
-        this.brief = brief;
-
-        this.author = os.userInfo().username;
-
-        this.date = formattedDate;
-    }
-
-    static fromJSON(json: string): TechDebt {
-        const obj = JSON.parse(json);
-        const td = new TechDebt(obj.brief);
-        return td;
-    }
+interface ITechDebt {
+    id?: string;
+    brief: string;
+    author?: string;
+    date?: string;
+    description?: string;
+    owner?: string;
+    category?: string;
+    severity?: string;
+    priority?: string;
+    file: string;
+    line?: number;
+    column?: number;
+    votes?: number;
+    workitem?: string;
+    cost?: string;
+    effort?: string;
+    impedes?: string;
+    discussion?: any[];
+    tags?: string[];
 }
 
 export class TechDebts {
@@ -60,7 +38,87 @@ export class TechDebts {
             this.getTechDebtsInWorkspace();
         });
 
+        context.subscriptions.push(vscode.commands.registerCommand('vstechdebt.addTechDebt', async (selectedResourceUri: vscode.Uri) => {
+            this.addTechDebt(selectedResourceUri);
+        }));
+
         this.getTechDebtsInWorkspace();
+    }
+
+    private async addTechDebt(uri: vscode.Uri) {
+
+        var brief = await vscode.window.showInputBox({
+            prompt: 'Enter a brief description for the technical debt:',
+            placeHolder: path.basename(uri.fsPath),
+        });
+
+        if (brief && (vscode.workspace.workspaceFolders !== undefined)) {
+            var tdFileName = brief.replace(/\s+/g, '-');
+            tdFileName = tdFileName.replace(/[^\w-]/gi, '') + ".tdr";
+
+            const currentDate = new Date();
+    
+            const day = currentDate.getDate().toString().padStart(2, "0");
+            const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+            const year = currentDate.getFullYear().toString();
+    
+            const formattedDate = `${year}-${month}-${day}`;
+
+            const techDebt: ITechDebt = {
+                id: "TDR_" + crypto.createHash('sha256').update(brief + currentDate).digest('hex').toUpperCase().substring(0, 8),
+                brief: brief,
+                author: os.userInfo().username,
+                date: formattedDate,
+                file: path.relative(vscode.workspace.workspaceFolders[0].uri.fsPath, uri.fsPath),
+                line: 0,
+                column: 0
+            };
+            const tdFilePath = path.join(path.dirname(uri.fsPath), ".tdr", tdFileName);
+
+            const jsonString = JSON.stringify(techDebt, null, 4);
+
+            fs.mkdir(path.dirname(tdFilePath), { recursive: true }, (err) => {
+                if (err) {
+                    vscode.window.showErrorMessage("Error creating directory: " + path.dirname(tdFilePath));
+                    return;
+                }
+
+                fs.writeFile(tdFilePath, jsonString, (err) => {
+                    if (err) {
+                        vscode.window.showErrorMessage("Error creating file: " + tdFilePath);
+                        return;
+                    }
+                    vscode.window.showInformationMessage("Sucessfully created " + tdFilePath + "!");
+                    this.createProblem(techDebt);
+                });
+            });
+
+        }
+    }
+
+    private loadTechDebt(json: ITechDebt): ITechDebt {
+        const techDebt: Required<ITechDebt> = {
+            id: json.id ?? "",
+            brief: json.brief,
+            author: json.author ?? "",
+            date: json.date ?? "",
+            description: json.description ?? "",
+            owner: json.owner ?? "",
+            priority: json.priority ?? "",
+            votes: json.votes ?? 0,
+            workitem: json.workitem ?? "",
+            cost: json.cost ?? "",
+            effort: json.effort ?? "",
+            impedes: json.impedes ?? "",
+            discussion: json.discussion ?? [],
+            tags: json.tags ?? [],
+            category: json.category ?? "techdebt",
+            severity: json.severity ?? "warning",
+            file: json.file ?? ".",
+            line: json.line ?? 0,
+            column: json.column ?? 0
+        };
+        return techDebt;
     }
 
     private getTechDebtsInWorkspace() {
@@ -84,7 +142,7 @@ export class TechDebts {
 
                         // Parse the TDR data
                         try {
-                            this.createProblem(TechDebt.fromJSON(data));
+                            this.createProblem(this.loadTechDebt(JSON.parse(data)));
                             console.log(`Successfully parsed TDR from file: ${fileName}`);
                         } catch (err) {
                             console.error(`Error parsing TDR from file: ${fileName}`);
@@ -98,15 +156,30 @@ export class TechDebts {
         }
     }
 
-    private createProblem(td: TechDebt) {
-        const diagnostic = new vscode.Diagnostic(
-            new vscode.Range(td.line, td.column, td.line, td.column), // Bereich des Problems
-            td.brief, // Beschreibung des Problems
-            vscode.DiagnosticSeverity.Warning // Schweregrad des Problems
-        );
-        const uri = vscode.Uri.file(td.file);
-        const diagnostics = vscode.languages.createDiagnosticCollection('TechDebts');
-        diagnostics.set(uri, [diagnostic]);
+    private createProblem(td: ITechDebt) {
+        if (vscode.workspace.workspaceFolders !== undefined) {
+            const diagnostic = new vscode.Diagnostic(
+                new vscode.Range(td.line || 0, td.column || 0, td.line || 0, td.column || 0),
+                td.brief
+            );
+            switch (td.severity) {
+                case "error":
+                    diagnostic.severity = vscode.DiagnosticSeverity.Warning;
+                    break;
+                case "information":
+                    diagnostic.severity = vscode.DiagnosticSeverity.Information;
+                    break;
+                case "hint":
+                    diagnostic.severity = vscode.DiagnosticSeverity.Hint;
+                    break;
+                default:
+                    diagnostic.severity = vscode.DiagnosticSeverity.Warning;
+                    break;
+            }
+            const uri = vscode.Uri.file(path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, td.file));
+            const diagnostics = vscode.languages.createDiagnosticCollection('TechDebts');
+            diagnostics.set(uri, [diagnostic]);
+        }
     }
 }
 
