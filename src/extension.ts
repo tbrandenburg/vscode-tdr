974015540f63f621dc20ff35a4a3bcf295af8ca7
btr8fe <tom.brandenburg@de.bosch.com>
2023-03-10 17:26:30 +0100

    Adapted README + removed console log

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,6 @@ import { TechDebts } from "./techdebt";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "vstechdebt" is now active!');
 	const td = new TechDebts(context);
 }
 
